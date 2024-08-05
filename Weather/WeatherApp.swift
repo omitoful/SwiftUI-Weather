dbcab2d8c5dd3770b09b4670499363e5759dfa34
Kevin <kevin@twjoin.com>
2024-08-05 14:58:42 +0800

    create project

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 陳冠甫 on 2024/8/5.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
