d4da3c5d306e261c512f2a34aab3e232023109f4
Kevin <kevin@twjoin.com>
2024-08-05 16:16:05 +0800

    basic view

--- Weather/ContentView.swift
+++ Weather/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .white],
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+            .ignoresSafeArea(.all)
+            
+            VStack {
+                Text("Cupertino, CA")
+                    .font(.system(size: 32, weight: .medium))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(.white)
+                }
+                
+                HStack {
+                    VStack {
+                        Text("TUE")
+                            .font(.system(size: 16, weight: .medium))
+                            .foregroundStyle(.white)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40, height: 40)
+                        Text("76°")
+                            .font(.system(size: 28, weight: .medium))
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
